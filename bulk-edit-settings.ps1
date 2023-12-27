6dd0aeff8102a71eb2b85fbe9d04c80aa656d3b7
Gabriel Balaich <GJBalaich@afstores.com>
2023-12-27 15:09:33 -0700

    Reverted previous changes, I didn't understand exactly how it worked

--- bulk-edit-settings.ps1
+++ bulk-edit-settings.ps1
@@ -140,12 +140,12 @@ function editRegistry($config) {
     }
   }
 
-  if($null -ne $($config)?.FileExplorer?.ShowFrequentlyUsedFoldersInHome){
-    $registryTweaks.ShowFrequentlyUsedFoldersInHome = [pscustomobject]@{
+  if($null -ne $($config)?.FileExplorer?.ShowFrequentlyUsedFoldersInQuickAccess){
+    $registryTweaks.ShowFrequentlyUsedFoldersInQuickAccess = [pscustomobject]@{
       path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer"
       property = "ShowFrequent"
       propertyType = "DWord"
-      propertyValue = $config.FileExplorer.ShowFrequentlyUsedFoldersInHome ? 1 : 0
+      propertyValue = $config.FileExplorer.ShowFrequentlyUsedFoldersInQuickAccess ? 1 : 0
     }
   }
 
